fd06218e5cb951d17033582b6e3c648b9544545b
Quentin <quentin.t@gmail.com>
2025-06-20 14:55:32 +0200

    Add hidden confetti button

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,49 @@
 <script setup lang="ts">
-import SplitFlapLine from './components/SplitFlapLine.vue';
-import {ref, useTemplateRef} from 'vue'
+import SplitFlapLine from './components/SplitFlapLine.vue'
+import {nextTick, ref, useTemplateRef} from 'vue'
 import {makeSyllablePack} from './lib/syllable.ts'
-import flapAudio from '/src/assets/flap.mp3';
+import flapAudio from '/src/assets/flap.mp3'
+import ConfettiExplosion from 'vue-confetti-explosion'
 
-const s = ref('ABC');
+const s = ref('')
 
-const audio = useTemplateRef<HTMLAudioElement>('audio');
+const audio = useTemplateRef<HTMLAudioElement>('audio')
 
 const onFlapStart = () => {
   if (!audio.value) {
-    return;
+    return
   }
-  audio.value.currentTime = 0;
-  audio.value.play();
-};
+  audio.value.currentTime = 0
+  audio.value.play()
+}
 
 const showNewSyllable = () => {
-  s.value = makeSyllablePack(2).toUpperCase();
-};
+  s.value = makeSyllablePack(2).toUpperCase()
+}
+
+const confetti = ref(false)
+
+const onSuccess = async (e: Event) => {
+  e.stopImmediatePropagation()
+
+  confetti.value = false
+  await nextTick()
+  confetti.value = true
+}
 </script>
 
 <template>
-  <div class="app-wrapper w-[100dvw] h-[100dvh] flex items-center justify-center" @click="showNewSyllable">
-    <SplitFlapLine v-if="s.length" :flap-ms="150"
-                   :text="s.toUpperCase()"
-                   @flap-start="onFlapStart" />
-  </div>
   <audio ref="audio" :src="flapAudio"></audio>
+  <div class="absolute inset-0 overflow-clip">
+    <ConfettiExplosion v-if="confetti" class="absolute left-1/2 top-1/2"/>
+  </div>
+  <div class="absolute inset-0 flex items-center justify-center" @click="showNewSyllable">
+    <SplitFlapLine v-if="s.length"
+                   :flap-ms="150"
+                   :text="s.toUpperCase()"
+                   @flap-start="onFlapStart"/>
+    <button class="absolute bottom-0 right-0 size-[10dvw]" @click="onSuccess"></button>
+  </div>
 </template>
 
 <style scoped lang="css">
